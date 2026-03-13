a518daa5755afa07159d28c5bb09f4095753a970
wwwromcomru-lang <www.rom.com.ru@gmail.com>
2026-03-13 19:40:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/suno5/',  // ← ТВОЁ ИМЯ РЕПО!
   server: {
     host: "::",
     port: 8080,
